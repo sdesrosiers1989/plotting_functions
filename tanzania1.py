2ab083b23421f9de7f965afa209b3d8564ba2e73
earsch <earsch@see3-95.leeds.ac.uk>
2018-11-12 14:50:40 +0000

    Added tanga function

--- tanzania1.py
+++ tanzania1.py
@@ -43,3 +43,28 @@ def tanzania_plot(ax, high):
         ax.add_feature(coast_10m, facecolor = 'none')
     else:
         ax.add_feature(cartopy.feature.COASTLINE)
+
+def tanga_plot(ax, high):   
+    
+    coast_10m = cfeature.NaturalEarthFeature('physical', 'coastline', '10m',
+                                         edgecolor = 'black')
+
+    fname = r'/nfs/see-fs-02_users/earsch/Documents/Leeds/Tanga Project/Data/GIS_Maps/Tanga'
+    tanga_feature = ShapelyFeature(Reader(fname).geometries(),
+                                   ccrs.PlateCarree(), edgecolor = 'black')
+    
+    ax.add_feature(cartopy.feature.BORDERS, linewidth = 0.5)
+    # set up latitude and longtiude ticks and format
+    ax.set_yticks([-6, -5, -4], crs=ccrs.PlateCarree())
+    lat_formatter = LatitudeFormatter()
+    ax.yaxis.set_major_formatter(lat_formatter)
+    ax.set_xticks([37,38, 39], crs=ccrs.PlateCarree())
+    lon_formatter = LongitudeFormatter()
+    ax.xaxis.set_major_formatter(lon_formatter)
+    # add tanga shapefile
+    ax.add_feature(tanga_feature, facecolor = 'none', linewidth = 0.5)
+    
+    if high == True:
+        ax.add_feature(coast_10m, facecolor = 'none')
+    else:
+        ax.add_feature(cartopy.feature.COASTLINE)
