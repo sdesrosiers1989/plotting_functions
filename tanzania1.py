f229672fe5e7079b8819758665613abf647895e2
earsch <earsch@see3-95.leeds.ac.uk>
2018-11-23 13:01:21 +0000

    Added general africa plot function

--- tanzania1.py
+++ tanzania1.py
@@ -86,6 +86,38 @@ def tanga_plot(ax, high, no_x = False, no_y = False, subset = False):
         ax.set_ylim([-6.25,-3.75])
         ax.set_xlim([-1, 1.25])
         
+def plot_africa(ax, high, no_x = False, no_y = False, Tanga = True,
+                yticks = [-10, -6, -2], xticks =[29, 34, 39]):   
+    
+    coast_10m = cfeature.NaturalEarthFeature('physical', 'coastline', '10m',
+                                         edgecolor = 'black')
+
+    fname = r'/nfs/see-fs-02_users/earsch/Documents/Leeds/Tanga Project/Data/GIS_Maps/Tanga'
+    tanga_feature = ShapelyFeature(Reader(fname).geometries(),
+                                   ccrs.PlateCarree(), edgecolor = 'black')
+    
+    ax.add_feature(cartopy.feature.BORDERS, linewidth = 0.5)
+    # set up latitude and longtiude ticks and format
+    
+    if no_y == False:
+        ax.set_yticks(yticks, crs=ccrs.PlateCarree())
+        lat_formatter = LatitudeFormatter()
+        ax.yaxis.set_major_formatter(lat_formatter)
+    
+    if no_x == False:
+        ax.set_xticks(xticks, crs=ccrs.PlateCarree())
+        lon_formatter = LongitudeFormatter()
+        ax.xaxis.set_major_formatter(lon_formatter)
+        
+    # add tanga shapefile
+    if Tanga == True:
+        ax.add_feature(tanga_feature, facecolor = 'none', linewidth = 0.5)
+    
+    if high == True:
+        ax.add_feature(coast_10m, facecolor = 'none')
+    else:
+        ax.add_feature(cartopy.feature.COASTLINE)
+        
 def extract_tanz(cube, circ = True):
     def tanzania_lat(input):
         return -12.0 < input < -0.75
